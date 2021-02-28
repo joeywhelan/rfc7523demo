f9286a9302f7a191e4cba49eeb730980e11bbf2d
Joey Whelan <joey.whelan@gmail.com>
2021-02-28 14:38:52 -0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview RFC7523 client
+ * @author Joey Whelan <joey.whelan@gmail.com>
+ */
+
+const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
+const issuer = "https://jwtissuer.dummy";
+const subject = "m2mclient";
+const audience = "https://server.dummy";
+
+(async () => {
+    const payload = { 
+        "iss": issuer,
+        "sub": subject,
+        "aud": audience,
+        "exp": Date.now() + 60000 //60 second expiration        
+    };
+    const alg = 'HS512'
+    const token = jwt.sign(payload, sharedKey, {algorithm: alg});
+    const authGrant = encodeURI('urn:ietf:params:oauth:grant-type:jwt-bearer');
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: `grant_type=${authGrant}&assertion=${token}`
+    });
+    
+    const json = await response.json();
+    console.log(`Results: ${JSON.stringify(json, null, 4)}`);
+})();
